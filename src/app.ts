09c3acd1850d943e8db9698795d2daa59cbc3541
src <spncrcrlsn@gmail.com>
2023-09-13 19:32:29 +0000

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = 52524;
 
 app.get('/', (req, res) => {
-    res.json({body: `The express server is running on port ${port}!`, married: false});
+    res.json({body: `We're running Express on port ${port}!`, married: false});
 });
 
 app.listen(port, () => {
